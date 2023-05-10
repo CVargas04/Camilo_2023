2f568d6c86d627b09bfb375ab85a209975ed601a
juancamilovargasmoreno <juancamilovargasmoreno46@gmail.com>
2023-05-10 09:04:02 -0500

    Initial commit

--- Codigo.py
+++ Codigo.py
@@ -7,9 +7,3 @@ class DatosMeteorologicos:
     def procesar_datos(self) -> Tuple[float, float, float, float, str]:
         with open(self.nombre_archivo, "r") as f:
             datos = f.readlines()
-
-        temperatura_total = 0
-        velocidad_viento_total = 0
-        temperatura_maxima = -273.15  # Temperatura más baja posible en grados Celsius
-        velocidad_viento_maxima = 0
-        direcciones_viento = []
