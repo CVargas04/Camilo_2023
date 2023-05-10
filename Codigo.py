521a77c3405d2153d8c50dcfc3c11450b78716a6
juancamilovargasmoreno <juancamilovargasmoreno46@gmail.com>
2023-05-10 08:50:40 -0500

    Initial commit

---
+++ Codigo.py
@@ -0,0 +1,17 @@
+from typing import Tuple
+
+
+class DatosMeteorologicos:
+    def __init__(self, nombre_archivo: str):
+        self.nombre_archivo = nombre_archivo
+
+    def procesar_datos(self) -> Tuple[float, float, float, float, str]:
+        with open(self.nombre_archivo, "r") as f:
+            datos = f.readlines()
+
+        temperatura_total = 0
+        velocidad_viento_total = 0
+        temperatura_maxima = -273.15  # Temperatura más baja posible en grados Celsius
+        velocidad_viento_maxima = 0
+        direcciones_viento = []
+
